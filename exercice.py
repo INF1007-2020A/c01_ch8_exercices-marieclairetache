2132830524378e5512a52d6a09ea37414e77168a
marieclairetache <marie-claire.tache@polymtl.ca>
2020-10-26 13:58:32 -0400

    example contexte manager

--- exercice.py
+++ exercice.py
@@ -1,3 +1,18 @@
+# example context manager :
+class ContextManagedList:
+    def __init__(self,1:list)-> None:
+        self.1 = 1
+
+    def __enter__(self): # assure qu'on fait cette opération avec d,ouvrire 
+        print("enter")
+        return self
+    
+    def __exist__(seld,exc_type,exc_val,exc_tb):#assure que certaines opérations sont effectué au moment ou on le désrier
+        #une fois que le code est fait et qu'on sort du fichier ce code vas rouler 
+        print("exit")
+
+with ContextManagedList([1,2])as my_list:
+    raise(Exception)
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
